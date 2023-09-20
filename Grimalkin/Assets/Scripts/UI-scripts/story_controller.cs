37ecb6e486046869704b832c276da26faca06588
Tim <88timryall@gmail.com>
2023-09-20 15:51:54 +1000

    fix: working sprites

--- Grimalkin/Assets/Scripts/UI-scripts/story_controller.cs
+++ Grimalkin/Assets/Scripts/UI-scripts/story_controller.cs
@@ -95,7 +95,7 @@ public class story_controller : MonoBehaviour
 
         ////// WHERE TO BEGIN ? ////////////
         // beging dialog 1
-        StartCoroutine(start_stage_9());
+        StartCoroutine(start_stage_1());
 
 
 
