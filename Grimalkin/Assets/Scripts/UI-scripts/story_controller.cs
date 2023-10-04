0c4942f91698e4e2433d0a518e39d50816b31f43
Tim <88timryall@gmail.com>
2023-10-04 13:37:44 +1000

    feat: add video player for sm ending

--- Grimalkin/Assets/Scripts/UI-scripts/story_controller.cs
+++ Grimalkin/Assets/Scripts/UI-scripts/story_controller.cs
@@ -89,7 +89,7 @@ public class story_controller : MonoBehaviour
 
         ////// WHERE TO BEGIN ? ////////////
         // beging dialog 1
-        StartCoroutine(start_stage_4());
+        StartCoroutine(start_stage_11());
 
        
 
