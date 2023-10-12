e5908ebefaa2723300d2dd982866b2da3a67a0f9
Tim <88timryall@gmail.com>
2023-10-12 15:35:30 +1000

    feat: highlight rainforest icon on hover

--- Grimalkin/Assets/Scripts/website-game/website_start_game_controler.cs
+++ Grimalkin/Assets/Scripts/website-game/website_start_game_controler.cs
@@ -4,22 +4,35 @@ using UnityEngine;
 
 public class website_start_game_controler : MonoBehaviour
 {
-    public GameObject computer;
+    public GameObject computer; // computer that the icon is displayed on
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    /*
+    when the user click on the rainforest icon, start the website up
+    */
     private void OnMouseDown()
     {
         computer.GetComponent<computer_controler>().start_game();
+    }
+
+    /*
+    when the users cursor is on the object change its colour to
+    notify them so they know what to click
+    */
+    void OnMouseEnter()
+    {
+        // turn half transparent (alpha = 0.5)
+        Color newColor = new Color(1, 1, 1, 0.25f); 
+        gameObject.GetComponent<SpriteRenderer>().material.color = newColor;
+    }
+
+    /*
+    when the users cursor is off the object change its colour to
+    notify them so they know what to click
+    */
+    void OnMouseExit()
+    {
+        // turn half transparent (alpha = 0.5)
+        Color newColor = new Color(1, 1, 1, 1);
+        gameObject.GetComponent<SpriteRenderer>().material.color = newColor;
     }
 }
